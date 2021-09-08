3de1d5f4b3c1210b49869727dc3bf9783710f360
Jihoon <68891494+JiHoonAHN@users.noreply.github.com>
2021-09-08 19:42:37 +0900

    [CREATE] Setting

--- bamboo-iOS/AppDelegate/SceneDelegate.swift
+++ bamboo-iOS/AppDelegate/SceneDelegate.swift
@@ -13,10 +13,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        
+        let mainViewController = LoginViewController()
+//        let navigationController = UINavigationController(rootViewController: mainViewController())
+
+        window?.backgroundColor = .white
+        window?.rootViewController = mainViewController
+        window?.makeKeyAndVisible()
+        for fontFaily in UIFont.familyNames{
+            for fontName in UIFont.fontNames(forFamilyName: fontFaily){
+                print(fontName)
+            }
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
---
+++ bamboo-iOS/Controller/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  bamboo-iOS
+//
+//  Created by Ji-hoon Ahn on 2021/09/08.
+//
+
+import UIKit
+
+class LoginViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
