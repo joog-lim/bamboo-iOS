97af04f7fedd86f9ac1195449b7857dfdd706fe2
JiHoonAhn <ahnjh1028@naver.com>
2021-09-23 23:38:17 +0900

    [Error]

--- bamboo-iOS/Controller/Rule/RuleViewController.swift
+++ bamboo-iOS/Controller/Rule/RuleViewController.swift
@@ -17,7 +17,18 @@ class RuleViewController: UIViewController{
         $0.translatesAutoresizingMaskIntoConstraints = false
     }
     
+    private let titleLabel = UILabel().then{
+        $0.textColor = .bamBoo_57CC4D
+        let text = "안녕하세요!"
+        let attributedStr = NSMutableAttributedString(string: text)
+        $0.dynamicFont(fontSize: 20, currentFontName: "NanumSquareRoundB")
+        attributedStr.addAttribute(.foregroundColor, value: UIColor.black, range: (text as NSString).range(of: "집"))
+        $0.attributedText = attributedStr
+    }
     
+
+
+
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,11 +44,18 @@ class RuleViewController: UIViewController{
     }
     func addView(){
         view.addSubview(backGroundScrollView)
+        view.addSubview(titleLabel)
     }
     func location(){
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.left.equalToSuperview()
+            
+        }
         backGroundScrollView.snp.makeConstraints {
             $0.top.left.right.bottom.equalToSuperview()
         }
+        
         backGroundScrollView.contentSize = CGSize(width: view.frame.width, height: bounds.height/0.938728)
     }
 }
