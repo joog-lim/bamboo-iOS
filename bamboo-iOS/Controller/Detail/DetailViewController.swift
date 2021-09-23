b7a3f5bdedef2ce983083e79f2b26474c926960a
JiHoonAhn <ahnjh1028@naver.com>
2021-09-23 15:26:15 +0900

    [CREATE] Title

--- bamboo-iOS/Controller/Detail/DetailViewController.swift
+++ bamboo-iOS/Controller/Detail/DetailViewController.swift
@@ -11,6 +11,12 @@ class DetailViewController : UIViewController {
     //MARK: - Properties
     let bounds = UIScreen.main.bounds
     
+    private let titleLabel = UILabel().then{
+        $0.text = "규칙"
+        $0.textColor = .bamBoo_57CC4D
+        $0.dynamicFont(fontSize: 20, currentFontName: "NanumSquareRoundB")
+    }
+    
     private let backGroundScrollView = UIScrollView().then{
         $0.backgroundColor = .clear
         $0.showsVerticalScrollIndicator = true
@@ -32,11 +38,17 @@ class DetailViewController : UIViewController {
     }
     func addView(){
         view.addSubview(backGroundScrollView)
+        backGroundScrollView.addSubview(titleLabel)
     }
     func location(){
         backGroundScrollView.snp.makeConstraints {
             $0.top.left.right.bottom.equalToSuperview()
         }
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(bounds.height/28)
+            $0.left.equalToSuperview().offset(bounds.width/18.75)
+        }
+        
         backGroundScrollView.contentSize = CGSize(width: view.frame.width, height: bounds.height/0.61796)
     }
 }
