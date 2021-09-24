051f2a4ddcda1e1281457bf880719dda8c2ffbc9
JiHoonAhn <ahnjh1028@naver.com>
2021-09-24 09:58:55 +0900

    [CREATE] Title Add

--- bamboo-iOS/Controller/Rule/RuleViewController.swift
+++ bamboo-iOS/Controller/Rule/RuleViewController.swift
@@ -19,12 +19,20 @@ class RuleViewController: UIViewController{
     
     private let titleLabel = UILabel().then{
         $0.textColor = .bamBoo_57CC4D
+        let string : NSMutableAttributedString = NSMutableAttributedString(string: "안녕하세요!이곳은")
+        string.setColorForText(textToFind: "이곳은", withColor: .black)
+        $0.attributedText = string
         $0.dynamicFont(fontSize: 20, currentFontName: "NanumSquareRoundB")
-        $0.text = "안녕하세요!"
     }
+    private let secondLineTitleLabel = UILabel().then{
+        $0.textColor = .bamBoo_57CC4D
+        let string : NSMutableAttributedString = NSMutableAttributedString(string: "광대숲입니다!")
+        string.setColorForText(textToFind: "입니다!", withColor: .black)
+        $0.attributedText = string
+        $0.dynamicFont(fontSize: 20, currentFontName: "NanumSquareRoundB")
+    }
+
     
-
-
 
     //MARK: - Lifecycle
     override func viewDidLoad() {
@@ -42,12 +50,16 @@ class RuleViewController: UIViewController{
     func addView(){
         view.addSubview(backGroundScrollView)
         backGroundScrollView.addSubview(titleLabel)
+        backGroundScrollView.addSubview(secondLineTitleLabel)
     }
     func location(){
         titleLabel.snp.makeConstraints {
-            $0.top.equalToSuperview()
-            $0.left.equalToSuperview()
-            
+            $0.top.equalToSuperview().offset(bounds.height/40.6)
+            $0.left.equalToSuperview().offset(bounds.width/26.786)
+        }
+        secondLineTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom)
+            $0.left.equalTo(titleLabel)
         }
         backGroundScrollView.snp.makeConstraints {
             $0.top.left.right.bottom.equalToSuperview()
