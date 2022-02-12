b3ea63c1825a32d9235756faa8198721402d6e94
JiHoonAhn <ahnjh1028@naver.com>
2022-02-12 22:14:46 +0900

    [CREATE] last Commit

--- bamboo-iOS/Sources/Scene/Main/User/Home/MainReactor.swift
+++ bamboo-iOS/Sources/Scene/Main/User/Home/MainReactor.swift
@@ -89,10 +89,17 @@ extension MainReactor{
             state.mainSection.items.removeAll()
             state.mainSection.items.append(contentsOf: sectionItem)
         case let .postEmoji(indexPath):
-//            state.mainSection.items[indexPath] = .main(.init(idx: 3, algorithmNumber: 3, title: "2", content: "1", tag: "3", reason: "12", createdAt: "2021-11-10T03:32:07.942Z", isClicked: false, emojiCount: 3))
-            print("index ---> [\(indexPath)] \(state.mainSection.items[indexPath])")
+            switch state.mainSection.items[indexPath]{
+            case let .main(data):
+                data.isClicked = true
+                data.emojiCount += 1
+            }
         case let .deleteEmoji(indexPath):
-            print("\(state.mainSection.items[indexPath])")
+            switch state.mainSection.items[indexPath]{
+            case let .main(data):
+                data.isClicked = false
+                data.emojiCount -= 1
+            }
         }
         return state
     }
