4a6efe9296cbb3032abd9da442830b3b92b58e9b
JiHoonAhn <ahnjh1028@naver.com>
2022-02-14 09:24:09 +0900

    [CREATE] reqiest

--- bamboo-iOS/Sources/Network/BamBooAPI+Request.swift
+++ bamboo-iOS/Sources/Network/BamBooAPI+Request.swift
@@ -11,9 +11,12 @@ import Then
 import KeychainSwift
 
 //MARK: - Error Type
+/// 토큰 만료 에러
+enum TokenError: Swift.Error {
+  case tokenExpired
+}
 enum BamBooAPIError : Error{
     case empty
-    case tokenExpired
     case requestTimeout(Error)
     case internetConnection(Error)
     case restError(Error, statusCode : Int? = nil, errorCode: String? = nil)
@@ -131,14 +134,22 @@ extension BamBooAPI{
         let requestString = "\(endpoint.method) \(endpoint.baseURL) \(endpoint.path)"
         
         return Self.moya.rx.request(endpoint)
-//            .flatMap{ // 401(Token Error)일때 다시 발급
-//                if $0.statusCode == 401{
-//                    throw BamBooAPIError.tokenExpired
-//                }else{
-//                    return Single.just($0)
-//                }
-//            }
+            .flatMap{ // 401(Token Error)일때 다시 발급
+                if $0.statusCode == 401{
+                    throw TokenError.tokenExpired
+                }else{
+                    return Single.just($0)
+                }
+            }
+            .retry(when: { (error : Observable<TokenError>) in
+                error.flatMap{ error -> Single<Response> in
+                    let provider : ServiceProviderType = ServiceProvider()
+                    return provider.loginService.postRefresh()
+                }
+            })
+            .handleResponse()
             .filterSuccessfulStatusCodes()
+            .retry(2)
             .catch(self.handleInternetConnection)
             .catch(self.handleTimeOut)
             .catch(self.handleREST)
@@ -170,8 +181,6 @@ extension BamBooAPI{
                         }
                         print(message)
                     }
-                case BamBooAPIError.tokenExpired:
-                    print("😇토큰 없어요!!")
                 default:
                     break
                 }
